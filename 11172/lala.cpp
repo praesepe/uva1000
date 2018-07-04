1f3a20dd7c4fd3f76ee7355f9e8b60bdd756ce24
lalayueh <lalayueh@gmail.com>
2018-07-05 02:18:04 +0800

    Added 11172

--- 11172/lala.cpp
+++ 11172/lala.cpp
@@ -1,10 +1,23 @@
+#include <cstdint>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    cout << s;
+    size_t number_of_case = 0;
+    cin >> number_of_case;
+
+    for (size_t i = 0; i < number_of_case; i++) {
+        int32_t a, b;
+        cin >> a >> b;
+        if (a > b) {
+            cout << ">" << endl;
+        } else if (a < b) {
+            cout << "<" << endl;
+        } else {
+            cout << "=" << endl;
+        }
+    }
+
     return 0;
 }
